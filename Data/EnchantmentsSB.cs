46d8d76b7b461f8085ac7d94d9c01a29cf9c29c6
Fernando Mauricio Chavarría Reyes <0253214@up.edu.mx>
2022-11-12 19:35:02 +0100

    Fixed the data base

---
+++ Data/EnchantmentsSB.cs
@@ -0,0 +1,15 @@
+﻿using LinqToDB.Mapping;
+
+namespace Data
+{
+    public class EnchantmentsDB
+    {
+        [PrimaryKey, Identity]
+        // We create a varibale for each column in the table
+        public int id { set; get; }
+        public string eName { set; get; }
+        public string eDescription { set; get; }
+        public string ePower { set; get; }
+        public string eTreasure { set; get; }
+    }
+}
--- Logic/Enchantments.cs
+++ Logic/Enchantments.cs
@@ -1,44 +1,50 @@
-﻿using Logic.Library;
-using System;
+﻿using Data;
+using LinqToDB;
+using Logic.Library;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
 namespace Logic
 {
     public struct EnchantmentProperties
-	{
-		public bool has_name;
-		public bool has_description;
-		public bool has_power;
-		public bool has_treasure;
+    {
+        public bool has_name;
+        public bool has_description;
+        public bool has_power;
+        public bool has_treasure;
         public EnchantmentProperties(bool name, bool description, bool power, bool treasure)
-		{
-			has_name = false;
-			has_description = false;
-			has_power = false;
-			has_treasure = false;
-		}
-	}
+        {
+            has_name = false;
+            has_description = false;
+            has_power = false;
+            has_treasure = false;
+        }
+    }
 
     public class Enchantments : EventsObjects
-	{
-		private List<TextBox> listOfProperties;
+    {
+        private List<TextBox> listOfProperties;
+        private EventsObjects eventsObjects = new EventsObjects();
 
-		public Enchantments(List<TextBox> listOfProperties)
-		{
-			this.listOfProperties = listOfProperties;
-		}
-		public Enchantments()
-		{
-
-		}
-        private void AddEnchantment()
-		{
-            
+        public Enchantments(List<TextBox> listOfProperties)
+        {
+            this.listOfProperties = listOfProperties;
         }
-	}
+        public Enchantments()
+        {
+
+        }
+        public void AddEnchantment(List<TextBox> properties) 
+        {
+            Connection db = new Connection();
+            db.Insert(new EnchantmentsDB()
+            {
+                eName = listOfProperties[0].Text,
+                eDescription = listOfProperties[1].Text,
+                ePower = listOfProperties[2].Text,
+                eTreasure = listOfProperties[3].Text
+            });
+        }
+    }
 }
