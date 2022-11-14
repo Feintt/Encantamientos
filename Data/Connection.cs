1dcf9ef8b94d59b52c43c5d09be10384b55fd5bb
Fernando Mauricio Chavarría Reyes <0253214@up.edu.mx>
2022-11-14 02:59:57 +0100

    Se añadio la opcion para eliminar datos

--- Data/Connection.cs
+++ Data/Connection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LinqToDB;
+﻿using LinqToDB;
 using LinqToDB.Data;
 
 namespace Data
@@ -11,6 +6,6 @@ namespace Data
     public class Connection : DataConnection
     {
         public Connection() : base("BooksDataBase") { }
-        public ITable<EnchantmentsDB> enchantmentsSB { get { return GetTable<EnchantmentsDB>();  } }
+        public ITable<EnchantmentsDB> enchantmentsSB { get { return GetTable<EnchantmentsDB>(); } }
     }
 }
